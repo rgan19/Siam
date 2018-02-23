944e7319c240773fdeda9effc4b6932398e24060
rgan19 <rgan19@yahoo.com.sg>
2018-02-23 10:50:54 +0800

    Upload Lobby

---
+++ SiamLobby/Assets/Scripts/HostGame.cs
@@ -0,0 +1,35 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour {
+
+	[SerializeField]
+	private uint roomSize = 10;
+
+	private string roomName;
+
+	private NetworkManager networkManager; 
+
+	void Start(){
+		networkManager = NetworkManager.singleton;
+		//to enable matchmaker
+		if (networkManager.matchMaker == null) {
+			networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetRoomName(string name){
+		roomName = name;
+	}
+
+	public void CreateRoom(){
+		if (roomName != "" && roomName != null) {
+			Debug.Log ("Creating Room: " + roomName + " with room for " + roomSize + " players.");
+			//Create room
+			networkManager.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+		}
+	}
+
+}
