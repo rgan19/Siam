94bef3e10b8edb13a78676259b73c07c9cdbeaca
Reuben <reuben_ho@hotmail.com>
2018-03-26 18:02:35 +0800

    Fixed player being able to phase out of map
    Fixed players able to phase out of map as well as interaction between
    players and shields.

--- SIAM/Assets/Scripts/PlayerScripts/Taichi.cs
+++ SIAM/Assets/Scripts/PlayerScripts/Taichi.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Taichi : MonoBehaviour {
     public float duration;
+    private Collider shield_collider;
     // Update is called once per frame
+    private void Start()
+    {
+        //set ignore collision with player?
 
+        shield_collider = GetComponent<Collider>();
+        GameObject[] allArrows = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < allArrows.Length; i++)
+        {
+            Physics.IgnoreCollision(allArrows[i].GetComponent<Collider>(), shield_collider);
+        }
+    }
     void Update () {
 
         duration -= 1 * Time.deltaTime;
