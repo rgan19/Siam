4bb872ac3b27174083f4fc92fab59d57cbc87f4d
cyruskyra <cyrus.wang.nian.yu@gmail.com>
2018-04-20 07:34:17 +0800

    Added UI and other final elements

--- MergedSiam/Assets/Scripts/PlayerScripts/Arrow.cs
+++ MergedSiam/Assets/Scripts/PlayerScripts/Arrow.cs
@@ -29,7 +29,7 @@ public class Arrow : NetworkBehaviour {
         
     }
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 		
         oldVel = rb.velocity;
